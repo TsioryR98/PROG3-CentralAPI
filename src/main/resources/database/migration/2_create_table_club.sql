9426ab23a548230d5bb5af4cce2e0696d3d97d75
Mihajatiana <hei.mihajatiana@gmail.com>
2025-05-08 08:10:01 +0300

    feat: add table club

---
+++ src/main/resources/database/migration/2_create_table_club.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE club
+(
+    club_id          UUID              NOT NULL,
+    club_name        Varchar(200)      NOT NULL,
+    championship     championship_enum NOT NULL,
+    acronym          Varchar(10)       NOT NULL,
+    year_creation    TIMESTAMP         NOT NULL,
+    stadium          Varchar(200)      NOT NULL,
+    coach_name       Varchar(200)      NOT NULL,
+    nationality      Varchar(100)      NOT NULL,
+    scored_goals     Int DEFAULT 0,
+    conceded_goals   Int DEFAULT 0,
+    difference_goals Int DEFAULT 0,
+    clean_sheets     Int DEFAULT 0,
+    PRIMARY KEY (club_id, championship)
+)
